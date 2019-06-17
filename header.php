3f1bc160a4339ad8d076a1dd17e6413ed49530a8
Emse <emsender@web.de>
2019-06-17 17:14:51 +0200

    first template

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+
+  include_once 'includes/db.php';
+  include_once 'includes/functions.php';
+
+?>
+
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Markowski, Maryschok, Tchudjo, Steffens, Zöller">
+
+    <title>
+      <?=$title?>  
+    </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="libs/css/bootstrap.min.css">
+    <link href="css/bs_overwrite.css" rel="stylesheet"> <!-- Änderungen am Bootstrap-Code durch Überschreiben in separater Datei  -->
+
+    <?php   
+        if (isset($cssFile)) { //$cssFile ist z.B. "index", dann wird index_style.css eingebunden
+          echo '<link rel="stylesheet" href="css/'.$cssFile.'_style.css">';
+        }
+        else {
+          echo '';
+        }
+    ?>
+
+  </head>
--- index.php
+++ index.php
@@ -1,79 +1,230 @@
 <?php
 
-include_once 'includes/db.php';
-    include_once 'includes/functions.php';
+  $cssFile = "index";
+  $title = "Railroad Ink";
+  include_once("header.php");
 
-    sec_session_start();
-    
-    if($mysqli->query("SELECT * FROM users")->num_rows == 0) {
-      header('Location: admin.php');
-      exit();
-    }
-    $title = "Anmelden";
-    include_once("header.php");
 ?>
 
-  <body class="text-center">
+  <body>
+    <div class="container">
 
-    <div class="form-signin">
-      <img src="images/logo.png" class="mb-4" width="72" height="72" alt="">
-      <h1 class="h3 mb-4 font-weight-normal">Immoboss Pro</h1>
-      <label for="inputEmail" class="sr-only">E-Mail-Adresse</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="E-Mail-Adresse" required autofocus>
-      <label for="inputPassword" class="sr-only">Passwort</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Passwort" required>
+      <div class="row"><!-- Übersichten -->
+        <div class="col-md-12" style="background-color: lightblue; min-height: 10rem">
 
-      <div class="my-4">
-        <a href="register.php">Als Eigentümer registrieren?</a>
+          <div class="row">
+            <div class="col-md-12">Spezialstrecken</div>
+          </div>
+
+          <div class="row" style="border: 1px dotted black;">
+            <div class="col-md-12">Würfelübersicht</div>
+          </div>
+
+          <div class="row" style="border: 1px dotted black;">
+            <div class="col-md-12">Wertungszeile</div>
+          </div>
+
+          <div class="row" style="border: 1px dotted black;">
+            <div class="col-md-12">Punkteübersicht</div>
+          </div>          
+
+        </div>
       </div>
 
-      <button class="btn btn-lg btn-primary btn-block" onclick="login()">Anmelden</button>
-      
-      <div class="alert alert-danger" id="errortext" role="alert" style="display: none">
-      </div>
-    </div>
 
-    <footer class="footerfixed text-muted">&copy; ProPra'19 Gruppe 3</footer>
+      <div class="row"><!-- 7x7-Felder -->
+        <div class="col-md-12" style="background-color: blue; min-height: 10rem">
+
+          <?php $fieldnr=1 ?>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+            <div class="col">
+              <?php echo ($fieldnr++) ?>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+    </div><!-- /container -->
+
+
+    <footer class="container text-muted">
+      <hr>
+      &copy; ProPra'19 Gruppe 3
+    </footer>
+
   </body>
 
 
-  <script src="js/sha512.min.js"></script>
-  <script src="js/jquery-3.4.1.min.js"></script>
+  <?php
+    include_once ("footer_bs.php");
+  ?>
 
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <script src="js/index.js"></script>
 
-  <script>
 
-    $('input').on('keypress', (event)=> {
-      if(event.which === 13){
-        $('.btn').click();
-      }
-    });
-
-    function errorMessage(text) {
-      $('#errortext').show(100);
-      $('#errortext').html(text);
-    }
-
-    function login() {
-
-      let password = $('#inputPassword').val(),
-          email = $('#inputEmail').val();
-
-      if (!email || !password) {
-        errorMessage("Bitte geben Sie eine E-Mail und das zugehörige Passwort an");
-        return;
-      }
-
-      $.post('includes/login.php',{email:email, password:sha512(password)}).done((data) => {
-          if(data == "1") {
-            window.location.href = './frontend';
-          } else {
-            errorMessage(data);
-          }
-      }).fail((data) => console.log(data));
-    }
-  </script>
 
 </html>
