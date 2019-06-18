f5cb815bcbf6858d4fc71cb138919303af42e279
Christian Zöller <christianzoeller98@gmail.com>
2019-06-18 19:21:30 +0200

    Erstellen der Hauptdatei für das Spiel.

---
+++ src/games/RailroadInk/RailroadInk.java
@@ -0,0 +1,153 @@
+package games.RailroadInk;
+
+/* -- IMPORTS -- */
+
+//classes that are necessary for the server
+import gameClasses.Game;
+import gameClasses.GameState;
+import global.FileHelper;
+
+//classes that are necessary for the user management
+import userManagement.User;
+
+//java classes
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RailroadInk extends Game {
+
+	/* -- ATTRIBUTES -- */
+	
+	//a user object is used to denote the user whose turn it is (might be useless)
+	private User playerTurn = null;
+	
+	//two ArrayLists are used to store the players and the spectators
+	private ArrayList<User> playerList = new ArrayList<User>();
+	private ArrayList<User> spectatorList = new ArrayList<User>();
+	
+	//an integer value counts the turns
+	private int turnCounter = 0;
+	
+	//a String is used to indicate if a player has left
+	private String playerLeft = null;
+	
+	/* -- METHODS NECESSARY FOR THE SERVER -- */
+	
+	@Override
+	public String getSite() {
+		// TODO load the HTML-File of the game
+		// (see page 12 of the tutorial)
+		return null;
+	}
+
+	@Override
+	public String getCSS() {
+		// TODO load the CSS-File(s?) of the game
+		// (see page 13 of the tutorial)
+		return null;
+	}
+
+	@Override
+	public String getJavaScript() {
+		// TODO load the JS-File(s?) of the game
+		// (see page 13 of the tutorial)
+		return null;
+	}
+
+	@Override
+	public int getMaxPlayerAmount() {
+		//the game is supposed to be played by a maximum number of 6 players
+		return 6;
+	}
+
+	@Override
+	public int getCurrentPlayerAmount() {
+		//return the amount of users currently stored in the list of players
+		return playerList.size();
+	}
+
+	@Override
+	public void execute(User user, String gsonString) {
+		// TODO implement what should happen if the user interacts with the game
+		
+	}
+
+	@Override
+	public ArrayList<User> getPlayerList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<User> getSpectatorList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGameData(String eventName, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		//TODO this is just a first version of the method, it is not finished yet
+		
+		//the user is only allowed to join the game, if there are less than 6 players and he is not already in the list
+		if (playerList.size() < getMaxPlayerAmount() && !playerList.contains(user)) {
+			playerList.add(user);
+		}
+		
+		
+		//if there are six players, start the game 
+		if(playerList.size() == 6) {
+			this.gState = GameState.RUNNING;
+		}
+	}
+
+	@Override
+	public void addSpectator(User user) {
+		// TODO Auto-generated method stub
+		this.spectatorList.add(user);
+	}
+
+	@Override
+	public boolean isJoinable() {
+		//TODO this is just a first version of the method, it is not finished yet
+		
+		//if there are 6 or more players, you cannot join
+		if(playerList.size() >= 6) {
+			return false;
+		} 
+		
+		//if the game is running, you can only join as a spectator
+		if(this.gState == GameState.RUNNING) {
+			//TODO add the player as a spectator
+			return false;
+		}
+		
+		//if the game is finished or closed, you cannot join
+		if(this.gState == GameState.CLOSED || this.gState == GameState.FINISHED) {
+			return false;
+		}
+		
+		//in all other cases, the game is joinable
+		return true;
+	}
+
+	@Override
+	public void playerLeft(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GameState getGameState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
