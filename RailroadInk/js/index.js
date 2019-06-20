9add7c75c74bd9b5fe694e154c3644f7bddf5287
Emse <emsender@web.de>
2019-06-20 23:54:52 +0200

    board vertically maximized

--- RailroadInk/js/index.js
+++ RailroadInk/js/index.js
@@ -1,13 +1,20 @@
 function insertFieldnumber() {
 
 	for (var i = 1; i <= 49; i++) {
-		document.getElementById(i).innerHTML = i;		
+
+		document.getElementById("field_"+i).innerHTML = ''+i;
 	}    
 }
 
-function removeFieldnumber() {
+function toogleFieldnumber() {
 
 	for (var i = 1; i <= 49; i++) {
-		document.getElementById(i).innerHTML = '';		
+		if (document.getElementById("field_"+i).innerHTML != '') {
+			document.getElementById("field_"+i).innerHTML = '';
+		}
+		else {
+			document.getElementById("field_"+i).innerHTML = ''+i;
+		}
+		
 	}    
 }
